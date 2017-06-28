795caf9e1441235698d820ccac0d397a57edeb42
Eric <erickanarr@gmail.com>
2017-06-27 21:51:11 -0400

    2nd commit

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
-// console.log("talking");// 1. Fetch your users data
-// 2. Loop over the data
-// 3. Display it in the `.customers` element
+'use strict';
 
+(function () {
 
-(function (){
+fetch("https://randomuser.me/api/?results=12&nat=us")
+  .then(
+    function(response) {
+      if (response.status !==200) {
+        console.log("Looks like there was a problem. Status Code: " + response.status);
+        return;
+      }
 
-  'use strict';
+      response.json().then(function(data) {
+        for (let i = 0; i < 12; i++) {
 
-  // Your Code Goes Here
+        var firstName = data.results[i].name.first;
+        var lastName = data.results[i].name.last;
+        var email = data.results[i].email;
+        var streetAddress = data.results[i].location.street;
+        var city = data.results[i].location.city;
+        var state = data.results[i].location.state;
+        var zipCode = data.results[i].location.postcode;
+        var phone = data.results[i].phone;
+        var ssn = data.results[i].id.value;
+        var profilePic = data.results[i].picture.large;
+        var fullName = firstName + " " + lastName;
+        var cityStateZip = city + ", " + state + " " + zipCode;
 
-  for (let i = 0; i < 12; i++){
-    fetch('https://randomuser.me/api/')
-    .then(
-      function(response){
-       if (response.status !== 200){
-         console.log('looks like there is a ghost in the machine. status code: ' +
-         response.status);
-         return;
-  }
-  response.json().then(function(data){
-    
-  })
-}
-)
-)
+        var markup = `
+                <div class="flex-container">
+                   <ul>
+                      <li class="pic"><img src=${profilePic}></li>
+                      <li class="name">${fullName}</li>
+                      <li class="email">${email}</li>
+                      <li class="address1">${streetAddress}</li>
+                      <li class="address2">${cityStateZip}</li>
+                      <li class="phone">${phone}</li>
+                      <li class="ssn">${ssn}</li>
+                    </ul>
+                  </div>
+                  `
+          var gridContainer = document.querySelector("#grid-container").innerHTML += markup;
+          }
+        })
+      })
+})();
